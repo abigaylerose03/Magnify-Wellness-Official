433102cc7daff4a8b09df6d0fe3f2b27140d1b4d
unknown <harrison619@live.com>
2021-04-02 14:39:52 -0700

    added images in the merch section and updated button

--- merch/merch.js
+++ merch/merch.js
@@ -18,32 +18,18 @@ const merchItems = [
     {
         name: "hoodie",
         price: "$30",
-        imgSrc: "",
+        imgSrc: "../merch/hoddie1.png",
         itemLink: "",
     },
     {
         name: "stickers",
         price: "1 for $2, 2 for $3",
-        imgSrc: "",
+        imgSrc: "../merch/stickers.jpg",
         itemLink: "",
     },
-    {
-        name: "bullet journal",
-        price: "$10",
-        imgSrc: "",
-        itemLink: "",
-    },
+   
     
 ]
-
-
-
-
-
-
-
-
-
 
 
 
