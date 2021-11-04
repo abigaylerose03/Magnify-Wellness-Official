70244113362166ef0ee7fb1b6c7520411f976a12
Taichen Rose <jatr812@gmail.com>
2021-11-04 14:47:19 -0700

    Updating nav responsive

--- new/components/nav.js
+++ new/components/nav.js
@@ -14,7 +14,20 @@ function Nav() {
 
         {/* Links */}
         <div className={styles.links}>
-          <ul className="flex">
+          <div class="block md:hidden">
+            <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+              <svg
+                class="fill-current h-3 w-3"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <title>Menu</title>
+                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+              </svg>
+            </button>
+          </div>
+
+          <ul className="flex hidden md:block">
             <p href="/" className={styles.top_item}>
               <Link href="/">App</Link>
             </p>
