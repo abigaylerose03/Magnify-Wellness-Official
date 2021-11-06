206fe2d6d51bc78f65c01e2577f47246d27164db
Taichen Rose <jatr812@gmail.com>
2021-11-06 04:50:45 -0700

    Updated py
    Updated layout and path

--- components/layout.js
+++ components/layout.js
@@ -10,7 +10,7 @@ function Layout({children, title}) {
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
       <Nav />
-      <main>
+      <main className = "py-12">
           {children}
       </main>
       <Footer />
