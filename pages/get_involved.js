cb588884cc6d4c6558dd76e9089f4bdbe09fb779
Taichen Rose <jatr812@gmail.com>
2021-11-05 17:46:18 -0700

    Moved data to utils
    Moved data to utils for getting started, getting page ready

--- pages/get_involved.js
+++ pages/get_involved.js
@@ -1,94 +1,13 @@
-// Add another object, copy and paste below
-// When adding image path. Just add the name of the file, don't need to add complete path
-
-const get_involved = [
-  // First Object
-  {
-    Name: "Mental Health Changemakers Program",
-    Description:
-      "The mental health forever database is a curated database with 10+ categories and 1000+ resources about mental health. From BIPOC and LGBTQIA+ resources to music, blogs, and international resources, the mental health changemakers aim to raise mental health awareness and help people around the world.",
-    Image: "",
-    Buttons: [
-      // Button 1
-      {
-        Name: "View the Database",
-        Path: "",
-      },
-    ],
-  },
-  // Second Object
-  {
-    Name: "Magnify x Community",
-    Description:
-      "Come join our slack group of more than 200 members to talk about mental health, bond with others, learn about other organizations, ask for advice, and more!",
-    Image: "",
-    Buttons: [
-      // Button 1
-      {
-        Name: "Join Slack",
-        Path: "",
-      },
-    ],
-  },
-  // Third Object
-  {
-    Name: "Mentorship Program",
-    Description:
-      "The Magnify Mentorship team endeavors to assist students with personalized academic and career guidance through our team of incredible mentors. Mentees are matched based on their personal needs and given the opportunity to network with like-minded student leaders from around the world.",
-    Image: "",
-    Buttons: [
-      // Button 1
-      {
-        Name: "Request a Mentor",
-        Path: "",
-      },
-    ],
-  },
-  // Fourth Object
-  {
-    Name: "Ambassador Fellowship",
-    Description:
-      "Magnify Wellness is proud to present our new initiative, the Ambassador Fellowship Program! Our ambassadors will continue the conversation about mental health awareness, spreading Magnify Wellness' mission of equal mental health support for all. Be part of our movement and ensure that everyone realizes the free services and programs we offer. Incentives include volunteer hours, certificate of recognition, features on social media, letters of recommendation, internship referrals, and access to Magnify Wellness' social capital.",
-    Image: "",
-    Buttons: [
-      // Button 1
-      {
-        Name: "Join the Ambassadors",
-        Path: "",
-      },
-    ],
-  },
-  // Fifth Object
-  {
-    Name: "Magnify Wellness Team",
-    Description:
-      "Interested in joining one of our core teams at Magnify? We hire a variety of volunteers from artists to web developers. Help us grow closer to our mission of ensuring everyone gains access to mental health support!",
-    Image: "",
-    Buttons: [
-      // Button 1
-      {
-        Name: "View Open Positions",
-        Path: "",
-      },
-      // Button 2
-      {
-        Name: "Join the Team",
-        Path: "",
-      },
-    ],
-  },
-  // Sixth Object
-  {
-    Name: "Magnify You Chapter Program",
-    Description:
-      "Start your own Magnify You Chapter! Skip past all the hurdles of creating your own organization and receive guidance and mentorship to start creating impact right away. You will receive a free personalized journal, a limited-time only binder, service hours, and the ability to quickly enact positive change in your very own community. Applications are being reviewed on a rolling basis, but the binder will only be offered until end of 2021. Apply today!",
-    Image: "",
-    Buttons: [
-      // Button 1
-      {
-        Name: "Learn More",
-        Path: "",
-      },
-    ],
-  },
-];
+export default function GettingInvolved() {
+  return (
+    <div>
+      <Head>
+        <title>Magnify Wellness</title>
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+      </Head>
+      <Nav />
+      <main></main>
+      <Footer />
+    </div>
+  );
+}
