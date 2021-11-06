b621c93ed76684518eea6d2f070e5aeab6bc080a
Taichen <jatr812@gmail.com>
2021-11-06 10:47:47 -0700

    Adding in team data

---
+++ data/team_data/team_home.js
@@ -0,0 +1,76 @@
+export const team = [
+  {
+    // These are featured people on home page above the teams
+    person: [
+      {
+        name: "Abigayle Peterson",
+        jobTitle: "Founder & CEO",
+        description:
+          "Abigayle Peterson is a current freshman at Western Washington University in the Honors Program and National Science Foundation CS/M Scholars Program majoring in computer science. She is currently particpating in undergraduate research using machine-learning and AI to improve the The U.S education system's computer science curriculum. When she is not coding, you can find her eating Asian food with friends, hiking, or writing.",
+        image: "",
+      },
+      {
+        name: "Aditi Shinde",
+        jobTitle: "COO",
+        description:
+          "Aditi is a freshman at The University of Texas at San Antonio and is majoring in Information Systems. She is involved in several organizations and is passionate about encouraging more girls to join STEM fields to close the gender gap. In her free time she loves to bake cookies, draw, play the ukulele, and drink chai. Aditi loves being a part of Magnify because of the impact it has on young people.",
+        image: "",
+      },
+    ],
+
+    departments: [
+        {
+            departmentName: "Tech",
+            departmentImage: ""
+        },
+        {
+            departmentName: "Mental Health Changemakers",
+            departmentImage: ""
+        },
+        {
+            departmentName: "Art",
+            departmentImage: ""
+        },
+        {
+            departmentName: "Finance",
+            departmentImage: ""
+        },
+        {
+            departmentName: "Partnerships",
+            departmentImage: ""
+        },
+        {
+            departmentName: "Marketing",
+            departmentImage: ""
+        },
+        {
+            departmentName: "Events",
+            departmentImage: ""
+        },
+        {
+            departmentName: "Social Media",
+            departmentImage: ""
+        },
+        {
+            departmentName: "Writing",
+            departmentImage: ""
+        },
+        {
+            departmentName: "Mentorship",
+            departmentImage: ""
+        },
+        {
+            departmentName: "Research and Development",
+            departmentImage: ""
+        },
+        {
+            departmentName: "Logistics",
+            departmentImage: ""
+        },
+        {
+            departmentName: "Community Engagement",
+            departmentImage: ""
+        },
+    ],
+  },
+];
--- pages/team.js
+++ pages/team.js
@@ -3,7 +3,7 @@ import Layout from "../components/layout";
 export default function Team() {
   return (
     <Layout title = "Meet the Team">
-      
+      <h1 className="text-center text-5xl">Meet the Team</h1>
     </Layout>
   );
 }
