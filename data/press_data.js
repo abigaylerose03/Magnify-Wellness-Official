c99a13702f0dd6d387d3f1e5014eb0cf1d90dbdb
Taichen Rose <jatr812@gmail.com>
2021-11-06 05:24:22 -0700

    Press data

--- data/press_data.js
+++ data/press_data.js
@@ -1,138 +1,160 @@
 export const press = [
-    {
-        title: "Features",
-        docs: [
-            {
-                pressName = "Girl Genius",
-                articleName = "Girl Genius Magazine: Issue Five", 
-                date = "March 9, 2021",
-                buttonPath = "https://issuu.com/girlgeniusmagazine/docs/gg_issue_v__5_",
-            },
-            {
-                pressName = "The STEAM Foundation",
-                articleName = "Interview with Abigayle Peterson - Founder & CEO of Magnify Wellness", 
-                date = "February 21, 2021",
-                buttonPath = "https://www.youtube.com/watch?v=fKr6-Tleky8",
-            },
-            {
-                pressName = "Reinvented Magazine",
-                articleName = "Reinvented Magazine: Issue 7 Next Gen in STEM", 
-                date = "February 18, 2021",
-                buttonPath = "https://www.reinventedmagazine.com/product-page/seventh-issue-print-magazine",
-            },
-            {
-                pressName = "Western Washington University - Department of Computer Science",
-                articleName = "Finalists for the NCWIT Collegiate Award", 
-                date = "January 20, 2021",
-                buttonPath = "https://cs.wwu.edu/finalists-ncwit-collegiate-award",
-            },
-            {
-                pressName = "The Paris Project",
-                articleName = "Mental Health Heros: Story Telling with Abby from Magnify Wellness", 
-                date = "January 17, 2021",
-                buttonPath = "https://open.spotify.com/episode/6HG8GtFpWlmAn8EctSlRXG?si=28jhVAkKR1mm8VzsbJ4L7A",
-            },
-            {
-                pressName = "The Western Front Online",
-                articleName = "First-year Western Student Creates Mental Wellness App for Students", 
-                date = "December 6, 2020",
-                buttonPath = "https://www.westernfrontonline.com/2020/12/06/mentalwellness/",
-            },
-            {
-                pressName = "And This is How It Started Podcast",
-                articleName = "How it Started: Magnify Wellness with Abigayle Peterson", 
-                date = "November 23, 2020",
-                buttonPath = "https://open.spotify.com/episode/7wnJBKkr8XCH6UHIT4o6jH?si=Kid1T6p9Rk2m7UmNoPOJeg",
-            },
-            {
-                pressName = "HEARO UW",
-                articleName = "Recenter Yourself: Self Defense with Abigayle Peterson & Harrison Toppen Ryan", 
-                date = "November 15, 2020",
-                buttonPath = "https://www.youtube.com/watch?v=TouDp6taF8g&t=654s&ab_channel=HEAROInitiative",
-            },
-            {
-                pressName = "Western Washington University Today",
-                articleName = "Honors student Abigayle Peterson develops new wellness app for the Apple Store", 
-                date = "November 4, 2020",
-                buttonPath = "https://westerntoday.wwu.edu/features/honors-student-abigayle-peterson-develops-new-wellness-app-for-the-apple-store",
-            },
-            {
-                pressName = "STEMlights",
-                articleName = "STEMlights Newsletter Edition 21 -- Stem Spotlight: Abigayle Peterson", 
-                date = "October 4, 2020",
-                buttonPath = "https://stemlights.stemchats.org/edition/21.html",
-            },
-            {
-                pressName = "Channel Kindness",
-                articleName = "Magnifying the Importance of Mental Health", 
-                date = "September 6, 2020",
-                buttonPath = "https://www.channelkindness.org/magnifying-mental-health/",
-            },
-            {
-                pressName = "Lemonades Life Newsletter",
-                articleName = "Lemons & Life — Issue 4: Spotlight Mention — Magnify Wellness", 
-                date = "August 24, 2020",
-                buttonPath = "https://lemonsandlifeorg.substack.com/p/lemons-and-life-issue-4?utm_medium=email&utm_campaign=cta",
-            },
-            {
-                pressName = "STEAMChangersCA",
-                articleName = "STEAMChangers: Abigayle Peterson", 
-                date = "August 3, 2020",
-                buttonPath = "https://steampowerca.org/steamchangers/abbypeterson",
-            },
-            {
-                pressName = "The Positive Note",
-                articleName = "BIPOC Mental Health Month: The Power of Empathy", 
-                date = "July 13, 2020",
-                buttonPath = "https://medium.com/@thepositivenote/bipoc-mental-health-month-the-power-of-empathy-6c5ae52fcf10",
-            },
-            {
-                pressName = "The Scholarship Track",
-                articleName = "GHC Scholarship Interview – Abigayle Peterson", 
-                date = "June 21, 2020",
-                buttonPath = "https://scholarshiptrack.org/2020/06/21/ghc-abigayle-peterson/",
-            },
-        ],
-    },
-    {
-        title = "Workshops",
-        docs = [
-            {
-                pressName = "Stemology",
-                articleName = "Magnify CEO Hosts AI Hacks II App Development Workshop", 
-                date = "December 27, 2020",
-                buttonPath = "https://docs.google.com/presentation/d/1tgTKn69LekTrDeR-1FRFI9CaUwyy_Wo44MPZ9esJoF8/edit",
-            },
-            {
-                pressName = "Saluburious Organization",
-                articleName = "Magnify CEO Hosts IOS Development Workshop", 
-                date = "December 26, 2020",
-                buttonPath = "https://docs.google.com/presentation/d/1uzxXPeGZKtzD6C_tCEE-BCYgEHXL6pfUHNKQ55egkPU/edit#slide=id.g94c3a170b3_1_5",
-            },
-            {
-                pressName = "Helloo World Hackathon",
-                articleName = "Code with Empathy Workshop", 
-                date = "August 16, 2020",
-                buttonPath = "https://drive.google.com/file/d/17IWchn1O9RVLQw7f--BVjuZYRWWUBFqK/view?usp=sharing",
-            },
-            {
-                pressName = "She Creates Makeathon",
-                articleName = "Magnify CEO Speaks at She Creates Makeathon: Creating the Future Starts with You", 
-                date = "August 4, 2020",
-                buttonPath = "https://shecreates.carrd.co/#speakers",
-            },
-            {
-                pressName = "UnsinkableYouth",
-                articleName = "Unsinkable Stories: Abigayle Peterson", 
-                date = "July 23, 2020",
-                buttonPath = "https://weareunsinkable.com/leading-with-empathy-abigayle-peterson/",
-            },
-            {
-                pressName = "CodeHers Collective",
-                articleName = "Start Up Engineer Panelist", 
-                date = "April 5, 2021",
-                buttonPath = "https://codehers.co/?page_id=547",
-            },
-        ]
-    }
-]
+  {
+    title: "Features",
+    docs: [
+      {
+        pressName: "Girl Genius",
+        articleName: "Girl Genius Magazine: Issue Five",
+        date: "March 9, 2021",
+        buttonPath: "https://issuu.com/girlgeniusmagazine/docs/gg_issue_v__5_",
+      },
+      {
+        pressName: "The STEAM Foundation",
+        articleName:
+          "Interview with Abigayle Peterson - Founder & CEO of Magnify Wellness",
+        date: "February 21, 2021",
+        buttonPath: "https://www.youtube.com/watch?v=fKr6-Tleky8",
+      },
+      {
+        pressName: "Reinvented Magazine",
+        articleName: "Reinvented Magazine: Issue 7 Next Gen in STEM",
+        date: "February 18, 2021",
+        buttonPath:
+          "https://www.reinventedmagazine.com/product-page/seventh-issue-print-magazine",
+      },
+      {
+        pressName:
+          "Western Washington University - Department of Computer Science",
+        articleName: "Finalists for the NCWIT Collegiate Award",
+        date: "January 20, 2021",
+        buttonPath: "https://cs.wwu.edu/finalists-ncwit-collegiate-award",
+      },
+      {
+        pressName: "The Paris Project",
+        articleName:
+          "Mental Health Heros: Story Telling with Abby from Magnify Wellness",
+        date: "January 17, 2021",
+        buttonPath:
+          "https://open.spotify.com/episode/6HG8GtFpWlmAn8EctSlRXG?si=28jhVAkKR1mm8VzsbJ4L7A",
+      },
+      {
+        pressName: "The Western Front Online",
+        articleName:
+          "First-year Western Student Creates Mental Wellness App for Students",
+        date: "December 6, 2020",
+        buttonPath:
+          "https://www.westernfrontonline.com/2020/12/06/mentalwellness/",
+      },
+      {
+        pressName: "And This is How It Started Podcast",
+        articleName: "How it Started: Magnify Wellness with Abigayle Peterson",
+        date: "November 23, 2020",
+        buttonPath:
+          "https://open.spotify.com/episode/7wnJBKkr8XCH6UHIT4o6jH?si=Kid1T6p9Rk2m7UmNoPOJeg",
+      },
+      {
+        pressName: "HEARO UW",
+        articleName:
+          "Recenter Yourself: Self Defense with Abigayle Peterson & Harrison Toppen Ryan",
+        date: "November 15, 2020",
+        buttonPath:
+          "https://www.youtube.com/watch?v=TouDp6taF8g&t=654s&ab_channel=HEAROInitiative",
+      },
+      {
+        pressName: "Western Washington University Today",
+        articleName:
+          "Honors student Abigayle Peterson develops new wellness app for the Apple Store",
+        date: "November 4, 2020",
+        buttonPath:
+          "https://westerntoday.wwu.edu/features/honors-student-abigayle-peterson-develops-new-wellness-app-for-the-apple-store",
+      },
+      {
+        pressName: "STEMlights",
+        articleName:
+          "STEMlights Newsletter Edition 21 -- Stem Spotlight: Abigayle Peterson",
+        date: "October 4, 2020",
+        buttonPath: "https://stemlights.stemchats.org/edition/21.html",
+      },
+      {
+        pressName: "Channel Kindness",
+        articleName: "Magnifying the Importance of Mental Health",
+        date: "September 6, 2020",
+        buttonPath: "https://www.channelkindness.org/magnifying-mental-health/",
+      },
+      {
+        pressName: "Lemonades Life Newsletter",
+        articleName:
+          "Lemons & Life — Issue 4: Spotlight Mention — Magnify Wellness",
+        date: "August 24, 2020",
+        buttonPath:
+          "https://lemonsandlifeorg.substack.com/p/lemons-and-life-issue-4?utm_medium=email&utm_campaign=cta",
+      },
+      {
+        pressName: "STEAMChangersCA",
+        articleName: "STEAMChangers: Abigayle Peterson",
+        date: "August 3, 2020",
+        buttonPath: "https://steampowerca.org/steamchangers/abbypeterson",
+      },
+      {
+        pressName: "The Positive Note",
+        articleName: "BIPOC Mental Health Month: The Power of Empathy",
+        date: "July 13, 2020",
+        buttonPath:
+          "https://medium.com/@thepositivenote/bipoc-mental-health-month-the-power-of-empathy-6c5ae52fcf10",
+      },
+      {
+        pressName: "The Scholarship Track",
+        articleName: "GHC Scholarship Interview – Abigayle Peterson",
+        date: "June 21, 2020",
+        buttonPath:
+          "https://scholarshiptrack.org/2020/06/21/ghc-abigayle-peterson/",
+      },
+    ],
+  },
+  {
+    title: "Workshops",
+    docs: [
+      {
+        pressName: "Stemology",
+        articleName: "Magnify CEO Hosts AI Hacks II App Development Workshop",
+        date: "December 27, 2020",
+        buttonPath:
+          "https://docs.google.com/presentation/d/1tgTKn69LekTrDeR-1FRFI9CaUwyy_Wo44MPZ9esJoF8/edit",
+      },
+      {
+        pressName: "Saluburious Organization",
+        articleName: "Magnify CEO Hosts IOS Development Workshop",
+        date: "December 26, 2020",
+        buttonPath:
+          "https://docs.google.com/presentation/d/1uzxXPeGZKtzD6C_tCEE-BCYgEHXL6pfUHNKQ55egkPU/edit#slide=id.g94c3a170b3_1_5",
+      },
+      {
+        pressName: "Helloo World Hackathon",
+        articleName: "Code with Empathy Workshop",
+        date: "August 16, 2020",
+        buttonPath:
+          "https://drive.google.com/file/d/17IWchn1O9RVLQw7f--BVjuZYRWWUBFqK/view?usp=sharing",
+      },
+      {
+        pressName: "She Creates Makeathon",
+        articleName:
+          "Magnify CEO Speaks at She Creates Makeathon: Creating the Future Starts with You",
+        date: "August 4, 2020",
+        buttonPath: "https://shecreates.carrd.co/#speakers",
+      },
+      {
+        pressName: "UnsinkableYouth",
+        articleName: "Unsinkable Stories: Abigayle Peterson",
+        date: "July 23, 2020",
+        buttonPath:
+          "https://weareunsinkable.com/leading-with-empathy-abigayle-peterson/",
+      },
+      {
+        pressName: "CodeHers Collective",
+        articleName: "Start Up Engineer Panelist",
+        date: "April 5, 2021",
+        buttonPath: "https://codehers.co/?page_id=547",
+      },
+    ],
+  },
+];
