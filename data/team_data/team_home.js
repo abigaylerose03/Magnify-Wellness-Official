05e6c2f2e1c05e08a000bad6b9c92e6bd68057a0
Taichen Rose <jatr812@gmail.com>
2021-11-06 17:55:59 -0700

    Have departments page working
    Got the team page working

--- data/team_data/team_home.js
+++ data/team_data/team_home.js
@@ -7,69 +7,69 @@ export const team = [
         jobTitle: "Founder & CEO",
         description:
           "Abigayle Peterson is a current freshman at Western Washington University in the Honors Program and National Science Foundation CS/M Scholars Program majoring in computer science. She is currently particpating in undergraduate research using machine-learning and AI to improve the The U.S education system's computer science curriculum. When she is not coding, you can find her eating Asian food with friends, hiking, or writing.",
-        image: "",
+        image: "abigayle_peterson.jpg",
       },
       {
         name: "Aditi Shinde",
         jobTitle: "COO",
         description:
           "Aditi is a freshman at The University of Texas at San Antonio and is majoring in Information Systems. She is involved in several organizations and is passionate about encouraging more girls to join STEM fields to close the gender gap. In her free time she loves to bake cookies, draw, play the ukulele, and drink chai. Aditi loves being a part of Magnify because of the impact it has on young people.",
-        image: "",
+        image: "aditi_shinde.jpg",
       },
     ],
 
     departments: [
         {
             departmentName: "Tech",
-            departmentImage: ""
+            departmentImage: "tech.png"
         },
         {
             departmentName: "Mental Health Changemakers",
-            departmentImage: ""
+            departmentImage: "mental_health_changemakers.png"
         },
         {
             departmentName: "Art",
-            departmentImage: ""
+            departmentImage: "art.png"
         },
         {
             departmentName: "Finance",
-            departmentImage: ""
+            departmentImage: "finance.png"
         },
         {
             departmentName: "Partnerships",
-            departmentImage: ""
+            departmentImage: "partnership.png"
         },
         {
             departmentName: "Marketing",
-            departmentImage: ""
+            departmentImage: "marketing.png"
         },
         {
             departmentName: "Events",
-            departmentImage: ""
+            departmentImage: "events.png"
         },
         {
             departmentName: "Social Media",
-            departmentImage: ""
+            departmentImage: "social_media.png"
         },
         {
             departmentName: "Writing",
-            departmentImage: ""
+            departmentImage: "writing.png"
         },
         {
             departmentName: "Mentorship",
-            departmentImage: ""
+            departmentImage: "mentorship.png"
         },
         {
             departmentName: "Research and Development",
-            departmentImage: ""
+            departmentImage: "research.png"
         },
         {
             departmentName: "Logistics",
-            departmentImage: ""
+            departmentImage: "logistics.png"
         },
         {
             departmentName: "Community Engagement",
-            departmentImage: ""
+            departmentImage: "community_engagement.png"
         },
     ],
   },
--- pages/team.js
+++ pages/team.js
@@ -1,9 +1,73 @@
 import Layout from "../components/layout";
+import Image from "next/image";
 
-export default function Team() {
+import { team } from "../data/team_data/team_home";
+
+// Fetch data on client side
+export async function getStaticProps() {
+  return {
+    props: {
+      data: team,
+    },
+  };
+}
+
+export default function Team({ data }) {
   return (
-    <Layout title = "Meet the Team">
+    <Layout title="Meet the Team">
       <h1 className="text-center text-5xl pt-12">Meet the Team</h1>
+
+      {data.map((section) => (
+        <>
+          {/* Featured People */}
+          {section.person.map((person) => (
+            <div className="mt-12 text-center px-4 lg:flex lg:justify-center">
+              {/* Image */}
+              <div className="lg:mr-6 lg:mt-12">
+                <Image
+                  src={`/img/team/members/${person.image}`}
+                  width="200"
+                  height="200"
+                  className="rounded-full"
+                />
+              </div>
+
+              {/* Content */}
+              <div className="mt-4 md:max-w-xl md:m-auto md:mt-4 lg:m-0 lg:mt-4 lg:max-w-2xl">
+                <h1 className="text-4xl">{person.name}</h1>
+                <p className="italic text-2xl mt-2">{person.jobTitle}</p>
+                <p className="text-lg mt-2">{person.description}</p>
+              </div>
+            </div>
+          ))}
+
+          <h3 className="mt-24 text-5xl text-center">Departments</h3>
+          {/* List of Departments */}
+          <div className="sm:grid sm:grid-cols-2 sm:max-w-xl sm:m-auto md:grid-cols-3 md:max-w-4xl">
+            {section.departments.map((department) => (
+              <div className="border-2 border-yellow-200 w-56 h-80 md:w-64 mt-12 m-auto hover:cursor-pointer">
+                <a href="/">
+                  <div className="text-center">
+                    <Image
+                      src={`/img/team/department_images/${department.departmentImage}`}
+                      height="200"
+                      width="200"
+                    />
+                  </div>
+
+                  {/* Content */}
+                  <div className = "relative h-28">
+                    <hr className="border-2" />
+                    <p className="text-3xl text-center p-4">
+                      {department.departmentName}
+                    </p>
+                  </div>
+                </a>
+              </div>
+            ))}
+          </div>
+        </>
+      ))}
     </Layout>
   );
 }
